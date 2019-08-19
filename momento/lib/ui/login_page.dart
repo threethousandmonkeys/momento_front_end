07c14897851547c717f720046e6a2e1a771263ad
PiPi <qiniz@student.unimelb.edu.au>
2019-08-19 10:38:59 +1000

    add bubble indication painter

--- momento/lib/ui/login_page.dart
+++ momento/lib/ui/login_page.dart
@@ -137,6 +137,23 @@ class _LoginPageState extends State<LoginPage>
     _pageController = PageController();
   }
 
+  void showInSnackBar(String value) {
+    FocusScope.of(context).requestFocus(new FocusNode());
+    _scaffoldKey.currentState?.removeCurrentSnackBar();
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+      content: new Text(
+        value,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+            fontFamily: "WorkSansSemiBold"),
+      ),
+      backgroundColor: Colors.blue,
+      duration: Duration(seconds: 3),
+    ));
+  }
+
   Widget _buildMenuBar(BuildContext context) {
     return Container(
       width: 300.0,

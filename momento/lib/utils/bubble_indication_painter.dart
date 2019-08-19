399b18d881c668a2c70411369e94a798e9031eb9
PiPi <qiniz@student.unimelb.edu.au>
2019-08-19 10:49:09 +1000

    more feature on bubble indication painter

--- momento/lib/utils/bubble_indication_painter.dart
+++ momento/lib/utils/bubble_indication_painter.dart
@@ -1,3 +1,31 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+
+class TabIndicationPainter extends CustomPainter {
+  Paint painter;
+  final double dxTarget;
+  final double dxEntry;
+  final double radius;
+  final double dy;
+
+  final PageController pageController;
+
+  TabIndicationPainter(
+      {this.dxTarget = 125.0,
+        this.dxEntry = 25.0,
+        this.radius = 21.0,
+        this.dy = 25.0, this.pageController}) : super(repaint: pageController) {
+    painter = new Paint()
+      ..color = Color(0xFFFFFFFF)
+      ..style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+  }
+
+  @override
+  bool shouldRepaint(TabIndicationPainter oldDelegate) => true;
+}

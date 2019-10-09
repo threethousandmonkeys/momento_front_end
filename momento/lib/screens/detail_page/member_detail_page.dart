c7bbdae643e6cd8eb153e57e672001d8915ae9f5
PiPi <qiniz@student.unimelb.edu.au>
2019-10-09 17:11:03 +1100

    optimize member detail page

--- momento/lib/screens/detail_page/member_detail_page.dart
+++ momento/lib/screens/detail_page/member_detail_page.dart
@@ -18,16 +18,20 @@ class MemberDetailPage extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.all(20),
           children: <Widget>[
-            CachedNetworkImage(
-              imageUrl: member.photo,
-              placeholder: (context, url) => SpinKitCircle(
-                color: Colors.purple,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20),
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl: member.photo,
+                placeholder: (context, url) => SpinKitCircle(
+                  color: Colors.purple,
+                ),
+                errorWidget: (context, url, error) => Icon(Icons.error),
               ),
-              errorWidget: (context, url, error) => Icon(Icons.error),
             ),
             Center(
               child: Padding(
-                padding: const EdgeInsets.all(0),
+                padding: const EdgeInsets.only(bottom: 20),
                 child: Text(member.firstName,
                     style: TextStyle(
                   color: Colors.black,
@@ -37,66 +41,62 @@ class MemberDetailPage extends StatelessWidget {
                 ),
               ),
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Description: ",
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold)),
-                Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Text(member.description),
-                )
-              ],
+            Entry(
+              title: "Description",
+              content: member.description,
             ),
-            Row(
-              children: <Widget>[
-                Text("Gender: ",
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold)),
-                Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Text(member.gender),
-                )
-              ],
+            Entry(
+              title: "Gender",
+              content: member.gender,
             ),
-            Row(
-              children: <Widget>[
-                Text("Date of Birth: ",
-                style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold)),
-                Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Text(member.birthday.toString().split(' ')[0]),
-                )
-              ],
+            Entry(
+              title: "Date of Birth",
+              content: member.birthday.toString().split(' ')[0],
             ),
-            Row(
-              children: <Widget>[
-                Text("Date of Death: ",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold)),
-                Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Text(member.deathday.toString().split(' ')[0]),
-                )
-              ],
+            Entry(
+              title: "Date of Death",
+              content: member.deathday.toString().split(' ')[0],
             ),
 
-            UglyButton(
-              text: "Edit Your Profile",
-              height: 10,
-              onPressed: (){
-                print("aha");
-              },
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: UglyButton(
+                text: "Edit Your Profile",
+                height: 10,
+                onPressed: (){
+                  print("aha");
+                },
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Entry extends StatelessWidget {
+  final String title;
+  final String content;
+  Entry({this.title, this.content});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title + ": ",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+        ),
+        Text(content,
+          style: TextStyle(
+            fontSize: 16
+          ),
+        ),
+      ],
     );
   }
 }
